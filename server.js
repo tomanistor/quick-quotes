f13cd7f96f301374e0f99463d1e248704abd4200
tomanistor <toma.nistor@gmail.com>
2017-07-15 03:00:57 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+var db
+
+MongoClient.connect('mongodb://tomanistor:tomanistor@ds139262.mlab.com:39262/quick-quotes', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname +'/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
